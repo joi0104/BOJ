82b089a381d5429d25ebbda9f569652b7adcc775
joi0104 <joi0104@naver.com>
2020-03-10 21:27:50 +0900

    solve boj9251

---
+++ 9251.py
@@ -0,0 +1,11 @@
+import sys
+a = sys.stdin.readline().strip().upper()
+b = sys.stdin.readline().strip().upper()
+len_a = len(a)
+len_b = len(b)
+dp = [[0] * (len_b + 1) for _ in range(len_a + 1)]
+for i in range(1, len_a + 1):
+    for j in range(1, len_b + 1):
+        if a[i - 1] == b[j - 1]: dp[i][j] = dp[i - 1][j - 1] + 1
+        else: dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+print(dp[-1][-1])
