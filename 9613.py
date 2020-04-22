26d18ec26a76df4ac283049945a6209b682fde19
joi0104 <joi0104@naver.com>
2020-04-22 13:54:06 +0900

    solve 9613

--- 9613.py
+++ 9613.py
@@ -2,7 +2,5 @@ import itertools
 import math
 for _ in range(int(input())):
     n, *nums = map(int,input().split())
-    gcd = []
-    for i in itertools.combinations(nums,2): gcd.append(math.gcd(i[0],i[1]))
-    print(sum(gcd))
+    print(sum([math.gcd(i[0],i[1]) for i in itertools.combinations(nums,2)]))
 
