831a6b3cbfcdb4f0f2ad095014ccae92415cc9e7
joi0104 <joi0104@naver.com>
2020-07-08 15:07:01 +0900

    solve 2493

---
+++ stupidAlgorithm/2493.py
@@ -0,0 +1,11 @@
+n = int(input())
+top = [100000000] + list(map(int,input().split()))
+dp = [0]
+for i in range(1,n+1):
+    idx = i-1
+    while True:
+        if top[idx] >= top[i]:
+            dp.append(idx)
+            break
+        idx = dp[idx]
+print(*dp[1:])
