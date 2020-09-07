63075d0fbca157765617dd9dc46cc09a85f469eb
joi0104 <jjjjoi0104@gmail.com>
2020-09-08 01:45:34 +0900

    solve 15897

--- stupidAlgorithm/15897.py
+++ stupidAlgorithm/15897.py
@@ -2,7 +2,7 @@ n = int(input())
 x,y,ans = 0,n+1,0
 while x+1 < y:
     a = x+1
-    b = n//a if not n%a else n//a+1
-    ans += (a*(y-b)+b) if a!=b else a*(y-b)
+    b = n//a+(1 if n%a else 0)
+    ans += (a*(y-b)+(b if a!=b else 0))
     x,y = a,b
 print(ans)
