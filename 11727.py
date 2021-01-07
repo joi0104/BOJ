72bc6fd19a7f188717e39bf7009edca3c7d4c96f
joi0104 <jjjjoi0104@gmail.com>
2021-01-07 11:53:17 +0900

    solve 11727

--- 11727.py
+++ 11727.py
@@ -1,4 +1,4 @@
 n = int(input())
-answer = [1,3]
-for i in range(2,n): answer.append(answer[-1] + (answer[-2]*2))
-print(answer[n-1]%10007)
+dp = [0,1,3]
+for i in range(3,n+1): dp[i%3] = (dp[(i-1)%3]+dp[(i-2)%3]*2)%10007
+print(dp[n%3]%10007)
