f2449d419a7b6f8d59db8156af90194cf3bab964
joi0104 <jjjjoi0104@gmail.com>
2021-07-27 16:04:52 +0900

    solve 10775

--- 10775.py
+++ 10775.py
@@ -1,15 +1,29 @@
 import sys
 input = sys.stdin.readline
 
+def find(x):
+    if x == parents[x]:
+        return x
+    parents[x] = find(parents[x])
+    return parents[x]
+
+
+def union(x,y):
+    root_x = find(x)
+    root_y = find(y)
+    if root_x != root_y: parents[root_y] = root_x
+
+
 g = int(input())
 p = int(input())
-gate = [0] * g
+answer = 0
+parents = [i for i in range(g + 1)]
 
 for _ in range(p):
     gi = int(input())
-    if sum(gate[:gi]) >= gi:
-        print(sum(gate))
-        exit()
-    gate[gi - 1] += 1
+    parents_gi = find(gi)
+    if parents_gi == 0: break
+    union(parents_gi-1, parents_gi)
+    answer += 1
 
-print(sum(gate))
+print(answer)
